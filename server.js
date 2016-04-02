c4938ee41729469ff23bfdacff1ba57587108515
Chongchong Wang <chongchong1110@gmail.com>
2016-04-02 16:39:23 -0700

    server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname,'./client')));
+
+app.listen(8000,function () {
+	// body...
+	console.log('listening on 8000 -- friends');
+})
